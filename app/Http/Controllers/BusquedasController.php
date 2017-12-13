75a3fdcaef2d485af965cb3c258c0e62555158af
Garrarop <30799098+Garrarop@users.noreply.github.com>
2017-12-13 01:38:06 -0300

    weas arregladas

--- app/Http/Controllers/BusquedasController.php
+++ app/Http/Controllers/BusquedasController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Categorie;
 use Illuminate\Http\Request;
 
 class BusquedasController extends Controller
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,105 +2,70 @@
 @section('title')
   <title>Registrarme</title>
 @section('content')
-<div class="container">
-  <div class="d-flex justify-content-center">
-            <div class="card card-default">
-                <div class="card-header">Registrarme</div>
-                <div class="card-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}" onSubmit="return validarPasswd()">
-                        {{ csrf_field() }}
+  <div class="container">
+    <div class="d-flex justify-content-center">
+              <div class="card card-default">
+                  <div class="card-header">Registrarme</div>
+                  <div class="card-body">
+                      <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                          {{ csrf_field() }}
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="control-label">Nombre y apellido</label>
+                          <div class="form-group {{ $errors->has('name') ? ' has-error' : '' }}">
+                              <label for="name" class="control-label">Nombre y apellido</label>
 
-                            <div class="">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                              <div class="">
+                                  <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                                  @if ($errors->has('name'))
+                                      <span class="help-block">
+                                          <strong>{{ $errors->first('name') }}</strong>
+                                      </span>
+                                  @endif
+                              </div>
+                          </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="control-label">E-Mail</label>
+                          <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                              <label for="email" class="control-label">E-Mail</label>
 
-                            <div class="">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                              <div class="">
+                                  <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                                  @if ($errors->has('email'))
+                                      <span class="help-block">
+                                          <strong>{{ $errors->first('email') }}</strong>
+                                      </span>
+                                  @endif
+                              </div>
+                          </div>
 
-                        <div class="form-group">
-                            <label for="password" class="control-label">Contraseña</label>
+                          <div class="form-group">
+                              <label for="password" class="control-label">Contraseña</label>
 
-                            <div class="">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                              <div class="">
+                                  <input id="password" type="password" class="form-control" name="password" required>
 
-                                
-                           </div>
-                        </div>
 
-                        <div class="form-group">
-                            <label for="password-confirm" class="control-label">Confirmar contraseña</label>
+                             </div>
+                          </div>
 
-                            <div class="">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
-                        </div>
+                          <div class="form-group">
+                              <label for="password-confirm" class="control-label">Confirmar contraseña</label>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Registrarme
-                                </button>
-                            </div>
-                       </div> 
-                       <script>
-                        var p1 = document.getElementById("password").value;
-                        var p2 = document.getElementById("password-confirm").value;
-                        
-                        var espacios = false;
-                        var cont = 0;
-                        
-                        function validarPasswd() {
-                            while (!espacios && (cont < p1.length)) {
-                            if (p1.charAt(cont) == " "){
-                                espacios = true;
-                            }
-                            cont++;
-                            }
-                        ` `
-                            if (espacios){
-                                alert ("La contraseña no puede contener espacios en blanco");
-                                return false;
-                            }
+                              <div class="">
+                                  <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                              </div>
+                          </div>
 
-                            if (p1.length == 0 || p2.length == 0) {
-                                debugger;
-                                alert("Los campos de la contraseña no pueden quedar vacios");
-                                return false;
-                            }
-
-                            if (p1 != p2) {
-                                alert("Las contraseñas deben de coincidir");
-                                return false;
-                            } else {
-                                alert("Todo esta correcto");
-                                return true; 
-                            }
-                        }
-                       </script>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+                          <div class="form-group">
+                              <div class="col-md-6 col-md-offset-4">
+                                  <button type="submit" class="btn btn-primary">
+                                      Registrarme
+                                  </button>
+                              </div>
+                         </div>
+                      </form>
+                  </div>
+              </div>
+          </div>
+      </div>
 @endsection
